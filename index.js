fdf3518d84178d55f89ccf73af53e147a48689c0
PrabhleenKaur02 <prabhleen02info@gmail.com>
2024-08-06 12:30:18 +0530

    signup & signin route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoute = require('./routes/user');
 
 const app = express()
 const port = 4000;
+mongoose.connect('mongodb+srv://kaurprabhleen2002:EGiNeRa7yLhNuZc1@cluster0.ih4d0ql.mongodb.net/LifewithLeenBlog').then(e => console.log('MongoDB connected'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRoute);
 
 app.get('/', (req, res) => {
     res.render("home");
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const User = require('../models/user');
 const router = Router();
 
 router.get('/signin', (req, res) => {
@@ -8,6 +8,24 @@ router.get('/signin', (req, res) => {
 
 router.get('/signup', (req, res) => {
     return res.render("signup");
+})
+
+router.post('/signin', async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.matchPassword(email, password);
+
+    console.log("User", user);
+    return res.redirect("/"); 
+})
+
+router.post('/signup', async(req, res) => {
+    const { fullName, email, password } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password
+    });
+    return res.redirect('/');
 })
 
 module.exports = router;
